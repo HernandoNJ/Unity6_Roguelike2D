86be103109d5109944fee86c85056e391f98f013
HernandoNJ <hernandonj.unity@gmail.com>
2024-12-06 19:33:22 -0500

    game manager added

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.Tilemaps;
 using Random = UnityEngine.Random;
 
@@ -9,6 +10,13 @@ public class BoardManager : MonoBehaviour
         public bool IsPassable;
     }
 
+    // Array to store whether each cell in the grid is passable
+    // 2D array containing objects of type CellData
+    // There will be two indices to access it
+    // m_BoardData[0,0] is the first item of the first line,
+    // m_BoardData[1,3] the fourth item of the second line
+    private CellData[,] m_boardCellsData;
+    
     // Visual representation of the board.
     // Unity's Tilemap is used to draw 2D tile-based maps.
     [SerializeField] private Tilemap m_tilemap;
@@ -17,30 +25,19 @@ public class BoardManager : MonoBehaviour
     // For managing the tilemap and positioning objects.
     [SerializeField] private Grid m_Grid;
 
-    [SerializeField] private PlayerController m_PlayerController;
-
-    [SerializeField] private Tile[] groundTiles;
-    [SerializeField] private Tile[] wallTiles;
-    [SerializeField] private Tile[] blockingTiles;
-
     [SerializeField] private int width;
     [SerializeField] private int height;
+    
+    [SerializeField] private Tile[] groundTiles;
+    [SerializeField] private Tile[] blockingTiles;
 
-    // Array to store whether each cell in the grid is passable
-    // 2D array containing objects of type CellData
-    // There will be two indices to access it
-    // m_BoardData[0,0] is the first item of the first line,
-    // m_BoardData[1,3] the fourth item of the second line
-    private CellData[,] m_boardCellsData;
-
-    private void Start()
+    public void Init()
     {
         m_tilemap = GetComponentInChildren<Tilemap>();
         m_Grid = GetComponentInChildren<Grid>();
         m_boardCellsData = new CellData[width, height];
 
         SetTiles();
-        SpawnPlayer();
     }
 
     private void SetTiles()
@@ -60,7 +57,7 @@ public class BoardManager : MonoBehaviour
                 if (isWallTile)
                 {
                     // Wall tiles
-                    tile = wallTiles[Random.Range(0, wallTiles.Length)];
+                    tile = blockingTiles[Random.Range(0, blockingTiles.Length)];
                     m_boardCellsData[x, y].IsPassable = false;
                 }
                 else
@@ -75,23 +72,6 @@ public class BoardManager : MonoBehaviour
         }
     }
 
-    public CellData GetCellData(Vector2Int cellIndex)
-    {
-        // If cellIndex is out of the grid bounds, return null
-        if (cellIndex.x < 0 || cellIndex.x >= width ||
-            cellIndex.y < 0 || cellIndex.y >= height)
-        {
-            return null;
-        }
-
-        return m_boardCellsData[cellIndex.x, cellIndex.y];
-    }
-
-    private void SpawnPlayer()
-    {
-        m_PlayerController.Spawn(this, new Vector2Int(1, 1));
-    }
-
     /// <summary>
     /// Receives a cell index[,] as Vector2Int
     /// and returns a value to be used as a transform position
@@ -103,85 +83,15 @@ public class BoardManager : MonoBehaviour
         return m_Grid.GetCellCenterWorld((Vector3Int)cellIndex);
     }
 
-    // public void Init()
-    // {
-    //     InitializeBoardComponents();
-    //
-    //     int blockTileIndex = Random.Range(0, 2);
-    //     SetBoardTiles(blockTileIndex);
-    // }
-    //
-    // private void InitializeBoardComponents()
-    // {
-    //     m_tilemap = GetComponentInChildren<Tilemap>();
-    //     m_Grid = GetComponentInChildren<Grid>();
-    //     m_boardCellsData = new CellData[width, height];
-    //
-    //     SetMainCameraInitialPosition();
-    // }
-    //
-    // private void SetMainCameraInitialPosition()
-    // {
-    //     int x = height / 2;
-    //     int y = width / 2;
-    //     int z = -10;
-    //
-    //     Camera.main.transform.position = new Vector3(x, y, z);
-    // }
-    //
-    // private void SetBoardTiles(int wallIndexArg)
-    // {
-    //     for (int y = 0; y < height; ++y)
-    //     {
-    //         for (int x = 0; x < width; ++x)
-    //         {
-    //             Tile tile;
-    //             m_boardCellsData[x, y] = new CellData();
-    //
-    //             // Wall tiles when x,y == 0 || -1
-    //             if (x == 0 || y == 0 || x == width - 1 || y == height - 1)
-    //             {
-    //                 tile = blockingTiles[wallIndexArg];
-    //                 m_boardCellsData[x, y].IsPassable = false;
-    //             }
-    //             else
-    //             // Inner tiles
-    //             {
-    //                 tile = groundTiles[Random.Range(0, groundTiles.Length)];
-    //                 m_boardCellsData[x, y].IsPassable = true;
-    //             }
-    //
-    //             m_tilemap.SetTile(new Vector3Int(x, y, 0), tile);
-    //         }
-    //     }
-    // }
-    //
-    // public Vector3 CellIndexToWorldPosition(Vector2Int cellIndex)
-    // {
-    //     return m_Grid.GetCellCenterWorld((Vector3Int)cellIndex);
-    // }
-    //
-    // public CellData GetCellData(Vector2Int cellIndex)
-    // {
-    //
-    //     // Check if cellIndex is out of bounds of the board
-    //     // using width and height values
-    //     if (IsCellOutOfBounds(cellIndex))
-    //     {
-    //         Debug.LogError("Cell Index out of bounds");
-    //         return null;
-    //     }
-    //
-    //     // Return cell data at x,y index
-    //     return m_boardCellsData[cellIndex.x, cellIndex.y];
-    //
-    //     bool IsCellOutOfBounds(Vector2Int cellIndex)
-    //     {
-    //         return
-    //         cellIndex.x < 0 ||
-    //         cellIndex.y < 0 ||
-    //         cellIndex.x >= width ||
-    //         cellIndex.y >= height;
-    //     }
-    // }
+    public CellData GetCellData(Vector2Int cellIndex)
+    {
+        // If cellIndex is out of the grid bounds, return null
+        if (cellIndex.x < 0 || cellIndex.x >= width ||
+            cellIndex.y < 0 || cellIndex.y >= height)
+        {
+            return null;
+        }
+
+        return m_boardCellsData[cellIndex.x, cellIndex.y];
+    }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,27 +2,16 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // [SerializeField] private BoardManager boardManager;
-    // [SerializeField] private PlayerController playerController;
-    //
-    // public static GameManager Instance { get; private set; }
-    // public TurnManager TurnManager { get ; private set ; }
-    //
-    // private void Awake()
-    // {
-    //     if (Instance != null)
-    //     {
-    //         Destroy(gameObject);
-    //         return;
-    //     }
-    //
-    //     Instance = this;
-    // }
-    //
-    // private void Start()
-    // {
-    //     TurnManager = new TurnManager();
-    //     boardManager.Init();
-    //     playerController.SpawnPlayer(boardManager, new Vector2Int(1,1));
-    // }
+    public BoardManager boardManager;
+    public PlayerController playerController;
+
+    private TurnManager m_TurnManager;
+
+    private void Start()
+    {
+        m_TurnManager = new TurnManager();
+      
+        boardManager.Init();
+        playerController.Spawn(boardManager, new Vector2Int(1,1));
+    }
 }
