c2d2cabd600d9d7cc48c24fa029dbc213598d313
HernandoNJ <hernandonj.unity@gmail.com>
2024-12-06 22:59:25 -0500

    added OnTick event, reducing food count

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,6 +10,8 @@ public class GameManager : MonoBehaviour
 
     public TurnHandler TurnHandler { get; private set; }
 
+    private int m_FoodAmount = 100;
+    
     private void Awake()
     {
         if (Instance != null)
@@ -24,8 +26,19 @@ public class GameManager : MonoBehaviour
     private void Start()
     {
         TurnHandler = new TurnHandler();
+        
+        // When the OnTick event is triggered
+        // Call the OnTickHandler method
+        TurnHandler.OnTick += OnTickHandler;
 
         boardManager.Init();
         playerController.Spawn(boardManager, new Vector2Int(1, 1));
+    }
+
+    // OnTurnHappen
+    private void OnTickHandler()
+    {
+        m_FoodAmount -= 1;
+        Debug.Log($"Current food {m_FoodAmount}");
     }
 }
--- Assets/Scripts/TurnHandler.cs
+++ Assets/Scripts/TurnHandler.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class TurnHandler
 {
     private int m_TurnCount;
+    
+    // Only the class declaring the OnTick event can trigger it
+    public event System.Action OnTick;
 
     public TurnHandler()
     {
@@ -12,6 +15,10 @@ public class TurnHandler
     public void Tick()
     {
         m_TurnCount++;
+        
+        // Call, Invoke the callback methods registered to the OnTick event
+        OnTick?.Invoke();
+        
         Debug.Log("Player tick: " + m_TurnCount);
     }
 }
